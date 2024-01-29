377b107454d91f554cc4f8e878e4c8c13637e47a
wmh <_@wmh.us.com>
2024-01-28 19:46:26 -0500

    Expose ChatColor and GameState at the crate root

--- macroquest/src/lib.rs
+++ macroquest/src/lib.rs
@@ -1,5 +1,7 @@
 pub use macroquest_macros::plugin;
-pub use pluginapi::{Plugin, PluginHandler};
+
+pub use crate::pluginapi::{Plugin, PluginHandler};
+pub use crate::types::{ChatColor, GameState};
 
 mod pluginapi;
 pub mod types;
