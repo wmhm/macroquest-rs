9cb6519eb11ed42fa97710c29767e878ec5416ce
wmh <_@wmh.us.com>
2024-01-30 16:07:17 -0500

    correct the feature name

--- macroquest/src/lib.rs
+++ macroquest/src/lib.rs
@@ -3,11 +3,11 @@
 pub use macroquest_macros::plugin;
 
 #[doc(hidden)]
-#[cfg(not(docrs))]
+#[cfg(not(docsrs))]
 pub use macroquest_sys as ffi;
 
 #[doc(hidden)]
-#[cfg(docrs)]
+#[cfg(docsrs)]
 pub mod ffi {
     pub mod eqlib {
         pub struct PlayerClient;
