258efb6ad1d3f9433e3034e54c39d4eb58d80199
wmh <_@wmh.us.com>
2024-01-31 19:01:01 -0500

    Add an example using the low level API

---
+++ examples/MQRustLowLevel/src/lib.rs
@@ -0,0 +1,26 @@
+#![allow(non_snake_case)]
+#![warn(clippy::cargo)]
+#![warn(clippy::correctness)]
+#![warn(clippy::suspicious)]
+#![warn(clippy::complexity)]
+#![warn(clippy::perf)]
+#![warn(clippy::style)]
+#![warn(clippy::pedantic)]
+
+// use macroquest::eq;
+use macroquest::log::trace;
+use macroquest::plugin::Reason;
+
+const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+
+#[macroquest::plugin::main]
+fn main(reason: Reason) {
+    match reason {
+        Reason::Load => {
+            trace!(module = PKG_NAME, "module loaded")
+        }
+        Reason::Unload => {
+            trace!(module = PKG_NAME, "module unloaded")
+        }
+    };
+}
--- examples/README.md
+++ examples/README.md
@@ -3,3 +3,8 @@
 ## [skeleton](skeleton)
 
 A basic do-nothing example that just acts as a skeleton for a plugin.
+
+
+## [MQRustLowLevel](MQRustLowLevel)
+
+A basic do-nothing example that shows off the low level, "raw" plugin API.
