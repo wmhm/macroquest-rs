14b2ad629e07541170ae5316104cc05bb670f54d
wmh <_@wmh.us.com>
2024-02-02 11:53:45 -0500

    mutable borrow

--- macroquest/src/plugin.rs
+++ macroquest/src/plugin.rs
@@ -23,7 +23,7 @@
 //! #[macroquest::plugin::hooks]
 //! impl Plugin for MyPlugin {
 //!     fn on_incoming_chat(&self, line: &str, color: ChatColor) -> bool {
-//!         let l = self.last.write().unwrap();
+//!         let mut l = self.last.write().unwrap();
 //!         *l = Some(line.to_string());
 //!
 //!         false
