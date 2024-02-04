bf385950a62539be83b976c23b0f955e37e728aa
wmh <_@wmh.us.com>
2024-02-04 03:21:25 -0500

    make write_chat_color support ansi colors

--- macroquest/src/mq.rs
+++ macroquest/src/mq.rs
@@ -6,6 +6,8 @@ use std::marker::PhantomData;
 use std::path::Path;
 use std::sync::OnceLock;
 
+use cansi::{Color, Intensity};
+
 use crate::eq::ChatColor;
 use crate::ffi::mq as mqlib;
 
@@ -88,22 +90,84 @@ pub fn paths() -> &'static Paths<'static> {
 }
 
 #[allow(missing_docs)]
-pub fn write_chat_color<'a, S>(line: S, color: ChatColor)
-where
-    S: Into<Cow<'a, str>>,
-{
-    match line.into() {
-        Cow::Borrowed(s) => mqlib::write_chat_color(s, color.into()),
-        Cow::Owned(s) => mqlib::write_chat_color(s.as_str(), color.into()),
-    }
-}
-
-#[allow(missing_docs)]
 pub fn write_chat<'a, S>(line: S)
 where
     S: Into<Cow<'a, str>>,
 {
     write_chat_color(line, ChatColor::default());
+}
+
+#[allow(missing_docs)]
+pub fn write_chat_color<'a, S>(line: S, color: ChatColor)
+where
+    S: Into<Cow<'a, str>>,
+{
+    mqlib::write_chat_color(&colorize_line(line.into()), color.into());
+}
+
+fn colorize_line<'a, S>(line: S) -> Cow<'a, str>
+where
+    S: Into<Cow<'a, str>>,
+{
+    let line = line.into();
+    match memchr::memchr(b'\x1b', line.as_bytes()) {
+        Some(_) => {
+            cansi::v3::categorise_text(&line)
+                .iter()
+                .flat_map(|m| {
+                    // We ignore most of the ANSI codes, because WriteChatColor
+                    // doesn't support them in any meaningful way, but we can
+                    // support foreground colors.
+                    match m.fg {
+                        None => ["", "", "", m.text, ""],
+                        Some(fg) => [
+                            // Control character that signifies a color code
+                            "\x07",
+                            // If we have a "Faint" color, then we'll use the darker
+                            // variant of our colors.
+                            match m.intensity.unwrap_or(Intensity::Normal) {
+                                Intensity::Faint => "-",
+                                _ => "",
+                            },
+                            // Map the ANSI colors to the MacroQuest color codes.
+                            //
+                            // MacroQuest supports 10 color codes instead of the
+                            // standard 8, adding Purple and Orange. To support
+                            // these two colors we'll use a similiar color with the
+                            // blink ANSI code set.
+                            match fg {
+                                Color::Black | Color::BrightBlack => "b",
+                                Color::Green | Color::BrightGreen => "g",
+                                Color::Magenta | Color::BrightMagenta => {
+                                    match m.blink {
+                                        // Purple is blinking Magenta.
+                                        Some(true) => "p",
+                                        // Otherwise, regular Magenta.
+                                        _ => "m",
+                                    }
+                                }
+                                Color::Red | Color::BrightRed => "r",
+                                Color::Cyan | Color::BrightCyan => "t",
+                                Color::Blue | Color::BrightBlue => "u",
+                                Color::White | Color::BrightWhite => "w",
+                                Color::Yellow | Color::BrightYellow => match m.blink {
+                                    // Orangle is blinking Yellow.
+                                    Some(true) => "o",
+                                    // Otherwise, regular Yellow.
+                                    _ => "y",
+                                },
+                            },
+                            // The actual text wrapped by this ANSI color code.
+                            m.text,
+                            // The "reset back to the normal color" code.
+                            "\x07x",
+                        ],
+                    }
+                })
+                .collect()
+        }
+        None => line,
+    }
 }
 
 #[allow(missing_docs)]
@@ -129,7 +193,7 @@ impl io::Write for ConsoleWriter {
                 io::Error::new(io::ErrorKind::InvalidInput, e.to_string())
             })?;
 
-            write_chat(colorize(line.trim_end_matches('\n')));
+            write_chat(line.trim_end_matches('\n'));
             written += raw.len();
         }
 
@@ -139,66 +203,4 @@ impl io::Write for ConsoleWriter {
     fn flush(&mut self) -> io::Result<()> {
         Ok(())
     }
-}
-
-#[cfg(feature = "colors")]
-fn colorize(line: &str) -> String {
-    use cansi::{Color, Intensity};
-
-    cansi::v3::categorise_text(line)
-        .iter()
-        .flat_map(|m| {
-            // We ignore most of the ANSI codes, because WriteChatColor
-            // doesn't support them in any meaningful way, but we can
-            // support foreground colors.
-            match m.fg {
-                None => ["", "", "", m.text, ""],
-                Some(fg) => [
-                    // Control character that signifies a color code
-                    "\x07",
-                    // If we have a "Faint" color, then we'll use the darker
-                    // variant of our colors.
-                    match m.intensity.unwrap_or(Intensity::Normal) {
-                        Intensity::Faint => "-",
-                        _ => "",
-                    },
-                    // Map the ANSI colors to the MacroQuest color codes.
-                    //
-                    // MacroQuest supports 10 color codes instead of the
-                    // standard 8, adding Purple and Orange. To support
-                    // these two colors we'll use a similiar color with the
-                    // blink ANSI code set.
-                    match fg {
-                        Color::Black | Color::BrightBlack => "b",
-                        Color::Green | Color::BrightGreen => "g",
-                        Color::Magenta | Color::BrightMagenta => match m.blink {
-                            // Purple is blinking Magenta.
-                            Some(true) => "p",
-                            // Otherwise, regular Magenta.
-                            _ => "m",
-                        },
-                        Color::Red | Color::BrightRed => "r",
-                        Color::Cyan | Color::BrightCyan => "t",
-                        Color::Blue | Color::BrightBlue => "u",
-                        Color::White | Color::BrightWhite => "w",
-                        Color::Yellow | Color::BrightYellow => match m.blink {
-                            // Orangle is blinking Yellow.
-                            Some(true) => "o",
-                            // Otherwise, regular Yellow.
-                            _ => "y",
-                        },
-                    },
-                    // The actual text wrapped by this ANSI color code.
-                    m.text,
-                    // The "reset back to the normal color" code.
-                    "\x07x",
-                ],
-            }
-        })
-        .collect()
-}
-
-#[cfg(not(feature = "colors"))]
-fn colorize(line: &str) -> &str {
-    line
 }
