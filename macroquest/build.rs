3901a17d26627720d5070ba20de2c1b4514f5248
wmh <_@wmh.us.com>
2024-02-06 12:27:13 -0500

    remove wrong info for rerun-if-changed

--- macroquest/build.rs
+++ macroquest/build.rs
@@ -3,10 +3,6 @@ use std::{env, fs};
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=include/eqlib.h");
-    println!("cargo:rerun-if-changed=include/mq.h");
-    println!("cargo:rerun-if-changed=src/ffi/mod.rs");
-    println!("cargo:rerun-if-changed=src/ffi/eqlib.cc");
 
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
 
